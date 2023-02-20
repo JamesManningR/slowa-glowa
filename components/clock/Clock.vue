12375ea2acc9f15024d2412d79c580da27cf0735
James Manning <jamesmanningr98@gmail.com>
2023-02-20 22:22:02 +0000

    added clock component

---
+++ components/clock/Clock.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  seconds: number
+  tag: string
+}>(), {
+  tag: 'div',
+})
+
+const formatted = $computed(() => {
+  const minutes = Math.floor(props.seconds / 60)
+  const seconds = props.seconds % 60
+
+  return `${minutes}:${seconds}`
+})
+</script>
+
+<template>
+  <component :is="tag">
+    {{ formatted }}}
+  </component>
+</template>
