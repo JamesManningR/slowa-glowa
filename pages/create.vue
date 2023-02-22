bd2425fa3643b2260fd7f8a3e3c590333333ff66
James Manning <jamesmanningr98@gmail.com>
2023-02-22 22:44:51 +0000

    added bad create form

---
+++ pages/create.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const name = $ref('')
+
+let newWord = $ref('')
+const words = $ref([])
+
+const addWord = () => {
+  words.push(newWord)
+  newWord = ''
+}
+</script>
+
+<template>
+  <main>
+    <form>
+      <h1>Create Pack</h1>
+
+      <label for="name">Name</label>
+      <AppInput id="name" v-model="name" type="text" />
+
+      <label for="add-word">Add Word</label>
+      <AppInput id="add-word" v-model="newWord" type="text" />
+      <AppButton @click.prevent="addWord">
+        Add
+      </AppButton>
+
+      <h2>Words</h2>
+
+      <ul>
+        <li v-for="(word, index) in words" :key="word">
+          <AppInput v-model="words[index]" />
+          <AppButton class="bg-red-500 hover:bg-red-800">
+            <Icon name="mdi-trash-can" />
+          </AppButton>
+        </li>
+      </ul>
+    </form>
+  </main>
+</template>
