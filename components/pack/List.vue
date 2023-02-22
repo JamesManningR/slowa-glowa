5b538bb636d7e1f3c03f949c2ede480606d9debf
James Manning <jamesmanningr98@gmail.com>
2023-02-22 22:44:42 +0000

    added pack list and preview components

--- components/pack/List.vue
+++ components/pack/List.vue
@@ -10,5 +10,33 @@ if (packs.length === 0)
 </script>
 
 <template>
-  <PackPreview v-for="pack in packs" :key="pack.name" :pack="pack" />
+  <ul flex gap="4">
+    <li v-for="pack in packs" :key="pack.name">
+      <PackPreview :pack="pack" />
+    </li>
+    <li>
+      <NuxtLink
+        to="/create"
+        flex="~ col"
+        justify="center"
+        items="center"
+        gap="1"
+        w="50"
+        h="65"
+        p="10"
+        bg="green-600 hover:green-800"
+        transition="background-color transform"
+        transform="hover:scale-110"
+        z-index="hover:10"
+        border="white 5"
+        rounded="xl"
+        text-3xl
+        text-white
+        text-center
+      >
+        Create
+        <Icon name="mdi-plus-circle" />
+      </NuxtLink>
+    </li>
+  </ul>
 </template>
--- components/pack/Preview.vue
+++ components/pack/Preview.vue
@@ -7,7 +7,24 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink :to="`/game/${pack.name}`" p="4" bg="blue-600" text="white">
-    <h3>{{ pack.name }}}</h3>
+  <NuxtLink
+    :to="`/game/${pack.name}`"
+    flex
+    content="center"
+    items="center"
+    w="50"
+    h="65"
+    p="10"
+    bg="blue-600 hover:blue-800"
+    transition="background-color transform"
+    transform="hover:scale-110"
+    z-index="hover:10"
+    border="white 5"
+    rounded="xl"
+    text-3xl
+    text-white
+    text-center
+  >
+    {{ pack.name }}
   </NuxtLink>
 </template>
