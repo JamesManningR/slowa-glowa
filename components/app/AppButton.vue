ed1a979c991dd20a03917a36d5f2b0cd968a4643
James Manning <jamesmanningr98@gmail.com>
2023-03-16 23:05:10 +0000

    restyled button

--- components/app/AppButton.vue
+++ components/app/AppButton.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
 defineProps<{
-  disabled?: boolean
+  isLoading: boolean
 }>()
 </script>
 
 <template>
   <button
-    :disabled="disabled"
+    :aria-busy="isLoading"
+    :disabled="isLoading"
     p="x-6"
     uppercase
     tracking="wide"
     font="semibold"
-    border-2
-    text="black"
+    border="2"
+    bg="hover:bg-opacity-90 active:bg-opacity-75"
   >
     <slot />
+
+    <Icon v-if="isLoading" name="mdi-loading" animate="spin" />
   </button>
 </template>
