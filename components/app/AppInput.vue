2ef94f79c3abb2611501d2ad035f22da80b5e0e0
James Manning <jamesmanningr98@gmail.com>
2023-03-16 23:05:27 +0000

    used implicit modling to reduce input lag

--- components/app/AppInput.vue
+++ components/app/AppInput.vue
@@ -1,20 +1,13 @@
-<script setup lang="ts">
-import { computed } from 'vue'
-
-const props = defineProps<{
-  modelValue: string
-}>()
-
-const emit = defineEmits<{
-  (e: 'update:model-value', value: string): void
-}>()
-
-const value = computed({
-  get: () => props.modelValue,
-  set: (value: string) => emit('update:model-value', value),
-})
+<script setup lang="ts" inheritsAttr="false">
 </script>
 
 <template>
-  <input v-model="value" bg="transparent" transition="border-color ease" border="b-2 current focus:teal-400" py="2" outline="none">
+  <input
+    v-bind="$attrs"
+    bg="transparent"
+    transition="border-color ease"
+    border="b-2 current focus:teal-400"
+    py="2"
+    outline="none"
+  >
 </template>
