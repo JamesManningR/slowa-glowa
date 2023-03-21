72b478eac521996680aea38f52b18dc5a7acad4a
James Manning <jamesmanningr98@gmail.com>
2023-03-21 00:32:08 +0000

    made flipper more accessible

--- components/clock/Flipper.vue
+++ components/clock/Flipper.vue
@@ -6,9 +6,10 @@ defineProps<{
 </script>
 
 <template>
-  <div flex="~ col" items="center" p="2" bg="neutral" text="neutral-content" rounded="box">
+  <div flex="~ col" items="center" p="2" bg="neutral" text="neutral-content" rounded="box" role="timer">
     <span class="countdown" font="mono" text="5xl">
       <span :style="{ '--value': value }" />
+      <span class="sr-only">{{ value }}</span>
     </span>
     <span text="5">
       {{ label }}
